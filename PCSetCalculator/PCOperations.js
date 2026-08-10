3036d0e25d9bee1a216c03c23492c60f8be3ddda
blafleur327 <103611758+blafleur327@users.noreply.github.com>
2026-08-10 10:05:04 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- PCSetCalculator/PCOperations.js
+++ PCSetCalculator/PCOperations.js
@@ -1,5 +1,5 @@
 
-import { PCset,Serialism,DisplayTypes,mouseTracking, MyDropdown, IntervalLookup, findIntervals, MySynth} from "../pcMethods.js";
+import { PCset,Serialism,DisplayTypes,mouseTracking, MyDropdown, IntervalLookup, findIntervals} from "../pcMethods.js";
 
     /**
      * Returns the factors/divisors of an input.
@@ -72,7 +72,7 @@ function DrawingManager (parent = document.querySelector(`#drawing`)) {
         'subset': false,
         'superset': false,
     };
-    this.synthManager = new MySynth();
+    // this.synthManager = new MySynth();
     /**
      * Controls the display type, the visual format of the drawing.
      */
